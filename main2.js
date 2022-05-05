e39b1efeb19269aabd9239e6f9c94d8fbbcec733
Kaushal Rathod <krathod37@gmail.com>
2022-05-05 23:56:36 +0530

    new files added

--- main2.js
+++ main2.js
@@ -1,51 +1,51 @@
 //count number of characters in a string
-//
-// function palindrome(s){
-//   const obj = {}
-//   for (i = 0; i< s.length; i++){
-//     const char = s[i]
-//     if (!obj[char]) {
-//       obj[char] = 0
-//     }
-//     obj[char]++
-//     }
-//   return obj
-// }
-// console.log(palindrome('racecar'));
 
-// function autocomplete(input, dictionary){
-//   let arr = []
-//   let newInp = input.replace(/[^a-z]/g,'')
-//   console.log(newInp)
-//   if (newInp !== ''){
-//   dictionary.forEach (word => {
-//     newWord = word.replace(/[\W\d]/g,'')
-//     if (newWord.toLowerCase().startsWith(newInp)){
-//       arr.push(newWord)
-//     }
-//   })
-//   if (arr.length > 5){
-//     return arr.slice(0,5)
-//   }
-//  }
-//   return arr
-// }
-// console.log(autocomplete('ab*&1cd',['air','ae$#ro','ass']))
-//
-// // return a diamond shaped string
-// function diamond(n){
-//   if (n % 2 == 0 || n < 1){
-//     return null
-//   }
-//   let str = ''
-//   for(i = 0; i < n; i++){
-//     let len = Math.abs((n-2*i-1)/2)
-//     str += ' '.repeat(len)
-//     str += '*'.repeat(n-2*len)
-//     str += '\n'
-//     }
-//   return str
-// }
+function palindrome(s){
+  const obj = {}
+  for (i = 0; i< s.length; i++){
+    const char = s[i]
+    if (!obj[char]) {
+      obj[char] = 0
+    }
+    obj[char]++
+    }
+  return obj
+}
+console.log(palindrome('racecar'));
+
+function autocomplete(input, dictionary){
+  let arr = []
+  let newInp = input.replace(/[^a-z]/g,'')
+  console.log(newInp)
+  if (newInp !== ''){
+  dictionary.forEach (word => {
+    newWord = word.replace(/[\W\d]/g,'')
+    if (newWord.toLowerCase().startsWith(newInp)){
+      arr.push(newWord)
+    }
+  })
+  if (arr.length > 5){
+    return arr.slice(0,5)
+  }
+ }
+  return arr
+}
+console.log(autocomplete('ab*&1cd',['air','ae$#ro','ass']))
+
+// return a diamond shaped string
+function diamond(n){
+  if (n % 2 == 0 || n < 1){
+    return null
+  }
+  let str = ''
+  for(i = 0; i < n; i++){
+    let len = Math.abs((n-2*i-1)/2)
+    str += ' '.repeat(len)
+    str += '*'.repeat(n-2*len)
+    str += '\n'
+    }
+  return str
+}
 
 //break string input of camelCase into sentence with space between words
 
@@ -69,7 +69,7 @@ function breakCamelCase(string){
     return finalStr.trim()
   }
 
-//some mofos solution ==>
+//other simpler solution ==>
 function solution(string) {
   return(string.replace(/([A-Z])/g, ' $1'));
 }
